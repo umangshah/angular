e36719c596591ea05833471599a84dc45d87ceb2
Umang Shah <shahumangr@gmail.com>
2016-02-28 16:08:52 -0800

    AngularJs forms and validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -77,4 +77,31 @@ angular.module('confusionApp',[])
                 {
                     $scope.showDetails = !$scope.showDetails;
                 }
-        }]);
+        }])
+        .controller('ContactController', ['$scope', function($scope){
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                               agree:false, email:"" };
+            $scope.channels = [{value:"tel", label:'Tel.'},
+                               {value:"email",label:"Email"}];
+            $scope.invalidChannelSelection = false;
+                           
+        }])
+        .controller('FeedbackController', ['$scope', function($scope){
+            $scope.sendFeedback = function(){
+                console.log($scope.feedback);
+                 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+;
