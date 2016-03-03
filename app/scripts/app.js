c6a1389fad62d3ca7d0043f4913762d83f4edd4c
Umang Shah <shahumangr@gmail.com>
2016-03-02 19:36:23 -0800

    AngularJS UI router supports mutiple views and nested view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,66 @@
 'use strict';
 
-angular.module('confusionApp', ['ngRoute'])
-    .config(function($routeProvider){
-        $routeProvider
-            // route for contactus page
-            .when('/contactus', {
-                templateUrl : 'contactus.html',
-                controller : 'ContactController'
+angular.module('confusionApp', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+                    // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html'
+                    },
+                    'content': {
+                        template : '<h1>To be Completed</h1>',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html'
+                    }
+                }
             })
-            // route for menu page
-            .when('/menu', {
-                templateUrl : 'menu.html',
-                controller : 'MenuController'
+                    // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        template: '<h1>To be Completed</h1>',
+                        controller  : 'AboutController'
+                   }
+                }
             })
-            // route for dish detail page
-            .when('/menu/:id', {
-                templateUrl : 'dishdetail.html',
-                controller : 'DishDetailController'
+                    // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'
+                     }
+                }
             })
 
-            .otherwise('contactus');
-    })
-    
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+            $urlRouterProvider.otherwise('/');
+    })        
 ;
