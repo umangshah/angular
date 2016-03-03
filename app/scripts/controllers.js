506d35b7278d83d40bc656c1a5fa4ba2e280b8c4
Umang Shah <shahumangr@gmail.com>
2016-03-02 17:38:28 -0800

    Angular SPA using ngRoute, routeProvider, ngView and routeParams

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -67,8 +67,8 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-            $scope.dish = menuFactory.getDish(3);
+        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+            $scope.dish = menuFactory.getDish(parseInt($routeParams.id,10));
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
