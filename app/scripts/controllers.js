38f3bb0a5dad260b4dcfe0226a496924e0f4fdb5
Umang Shah <shahumangr@gmail.com>
2016-03-13 15:47:14 -0700

    use AngularJS ngResource module to tal to backend restful api server instead of $http

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,20 +7,10 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = false;
+            $scope.showMenu = true;
             $scope.message = "Loading ...";
-            $scope.dishes = {};
             
-            menuFactory.getDishes()
-                .then(
-                    function(response) {
-                        $scope.dishes = response.data;
-                        $scope.showMenu = true;
-                    },
-                    function(response) {
-                        $scope.message = "Error: " + response.status + " " + response.statusText;
-                    }
-            );
+            $scope.dishes = menuFactory.getDishes().query();
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -82,18 +72,8 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             $scope.message = "Loading ...";
-            $scope.showDish = false;
-            $scope.dish = {};
-            menuFactory.getDish(parseInt($stateParams.id,10))
-                .then(
-                    function(response) {
-                        $scope.dish = response.data;
-                        $scope.showDish = true;
-                    },
-                    function(response) {
-                        $scope.message = "Error: " + response.status + " " + response.statusText;
-                    }
-            );
+            $scope.showDish = true;
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -116,18 +96,8 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
         
             $scope.message = "Loading ...";
-            $scope.showDish = false;        
-            $scope.featuredDish = {};
-            menuFactory.getDish(0)
-                .then (
-                    function(response) {
-                        $scope.featuredDish = response.data;
-                        $scope.showDish = true;
-                    },
-                    function(response) {
-                        $scope.message = "Error: " + response.status+ " " + response.statusText;
-                    }
-            );
+            $scope.showDish = true;
+            $scope.featuredDish = menuFactory.getDishes().get({id:0});
             
             $scope.currentPromotion = menuFactory.getPromotion(0);
             
