5f5e57695f80cca834c1c8c1a34627c26cf9f06f
Umang Shah <shahumangr@gmail.com>
2016-03-13 16:10:31 -0700

    AngularJS - error handling with ngResource module and save user's comment about dish

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,10 +7,17 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = "Loading ...";
             
-            $scope.dishes = menuFactory.getDishes().query();
+            $scope.dishes = menuFactory.getDishes().query(
+                function(response) {
+                    $scope.dishes = response;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+            });
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -72,11 +79,20 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             $scope.message = "Loading ...";
-            $scope.showDish = true;
-            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+            $scope.showDish = false;
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+                .$promise.then(
+                            function(response){
+                                $scope.dish = response;
+                                $scope.showDish = true;
+                            },
+                            function(response) {
+                                $scope.message = "Error: "+response.status + " " + response.statusText;
+                            }
+                );
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
             
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
@@ -87,6 +103,8 @@ angular.module('confusionApp')
                 
                 $scope.dish.comments.push($scope.mycomment);
                 
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+                
                 $scope.commentForm.$setPristine();
                 
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
@@ -96,8 +114,17 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
         
             $scope.message = "Loading ...";
-            $scope.showDish = true;
-            $scope.featuredDish = menuFactory.getDishes().get({id:0});
+            $scope.showDish = false;
+            $scope.featuredDish = menuFactory.getDishes().get({id:0})
+                .$promise.then(
+                            function(response){
+                                $scope.featuredDish = response;
+                                $scope.showDish = true;
+                            },
+                            function(response) {
+                                $scope.message = "Error: "+response.status + " " + response.statusText;
+                            }
+                );
             
             $scope.currentPromotion = menuFactory.getPromotion(0);
             
